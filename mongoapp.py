073e23f6af2a3b03331d2d7891160d52d0148f62
aheyman <aheyman06@gmail.com>
2017-07-18 21:55:54 -0400

    Location URL

--- mongoapp.py
+++ mongoapp.py
@@ -1,6 +1,11 @@
 import json
+from pymongo import MongoClient
 from flask import Flask, current_app, render_template, Response, request
+from bson.son import SON
 app = Flask(__name__)
+client = MongoClient('localhost',27017)
+db = client['healthcare']
+tweets = db['healthcare']
 
 @app.route("/")
 def hello():
@@ -14,6 +19,18 @@ def update_data():
 
     js = json.dumps(data)
     resp = Response(js, status=200, mimetype='application/json')    
+    return resp
+
+@app.route('/location', methods = ['GET'])
+def retrieve_location():
+
+    location_query = [
+        {"$group":{"_id":"$user.location","total": {"$sum": 1}}},
+        {"$sort":SON({"total":-1})}
+    ]
+
+    js = json.dumps(tweets.aggregate(location_query))
+    resp = Response(js, status=200, mimetype='application/json')
     return resp
 
 if __name__ == "__main__":
